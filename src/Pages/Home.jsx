7c62e519476a84d48be8c3a8612bb8b2c1a6d77b
AnnieCannons Admin <anniecannonsadmin@AnnieCanonssMBP.attlocal.net>
2024-03-08 16:19:05 -0600

    adding to home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,15 +1,21 @@
 import Footer from "../Components/Footer";
 import NavBar from "../Components/NavBar";
 
-
 function Home() {
-
   return (
     <>
       <NavBar />
+      <div className="homeContainer">
+        <div className="leftSideContainer">
+          <p>I am Tanesha Berry, Front-end Developer, Executive Administrative Assistant, and Graphic Designer</p>
+        </div>
+        <div className="rightSideContainer">
+          
+        </div>
+      </div>
       <Footer />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
